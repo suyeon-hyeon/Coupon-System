8186ab074ada55f06b24dde010427f20be71bcaa
suyeon-hyeon <shonee9911@gmail.com>
2026-09-17 23:24:22 +0900

    [✨add] 쿠폰 발급 요청 처리 기능 개선 - 쿠폰 캐시 적용

--- coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.example.couponapi.service;
 
 import com.example.couponapi.controller.dto.CouponIssueRequestDto;
+import com.example.couponcore.service.AsyncCouponIssueServiceV1;
 import com.example.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -12,10 +13,15 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
         couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    public void asyncIssueRequestV1(CouponIssueRequestDto requestDto) {
+        asyncCouponIssueServiceV1.issue(requestDto.couponId(), requestDto.userId());
     }
 }
